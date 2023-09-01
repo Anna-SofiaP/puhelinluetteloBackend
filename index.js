4b8a69d4ab46b4c15ca67d2c5d6ae5eb979dbe57
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2023-09-01 09:20:28 +0300

    osa3 - puhelinluettelon backend: tehty tehtävät 3.4-3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       id: 1,
@@ -49,6 +51,44 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const newId = Math.floor(Math.random() * 100)
+    return newId
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({ 
+          error: 'Information is missing'
+        })
+    }
+
+    const hasSameName = persons.find(person => body.name === person.name)
+    if (hasSameName) {
+        return res.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 const PORT = 3001
