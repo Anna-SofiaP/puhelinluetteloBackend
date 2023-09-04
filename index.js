d07c451d2f8b9458183c003f0bb66ca803569621
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2023-09-04 10:03:03 +0300

    osa3 - puhelinluettelon backend: tehty 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', (req, res) => {return JSON.stringify(req.body)})
+app.use(cors())
 
 let persons = [
     {
@@ -94,7 +96,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
