e60b48e3c3f14c8192541b02142a335c1a89fa45
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2023-09-26 11:14:30 +0300

    osa3 - tehtävät 3.13-3.14 tehty

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -9,46 +11,7 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', (req, res) => {return JSON.stringify(req.body)})
 
-const mongoose = require('mongoose')
-
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-//Tähän npp:sta se tietokannan URL juttu!!!
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
 let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
 ]
 
 app.get('/api/persons', (req, res) => {
@@ -87,12 +50,29 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const generateId = () => {
+/*const generateId = () => {
     const newId = Math.floor(Math.random() * 100)
     return newId
-}
+}*/
 
 app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Information is missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+})
+
+/*app.post('/api/persons', (req, res) => {
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -116,9 +96,9 @@ app.post('/api/persons', (req, res) => {
 
     persons = persons.concat(person)
     res.json(person)
-})
+})*/
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
