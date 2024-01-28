81a11c3a676f82c3e1999f0d4655ad15ff83f7fe
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2024-01-28 11:06:00 +0200

    osa3 - tehtävä 3.19 tehty

--- index.js
+++ index.js
@@ -24,17 +24,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Phonebook</h1>')
 })
 
-/*app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(per => per.id === id)
-    
-  
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-})*/
 
 app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
@@ -70,38 +59,31 @@ app.delete('/api/persons/:id', (req, res) => {
     })
 })
 
-/*const generateId = () => {
-    const newId = Math.floor(Math.random() * 100)
-    return newId
-}*/
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
-  if (!body.name || !body.number) {
+  /*if (!body.name || !body.number) {
     return res.status(400).json({ error: 'Information is missing' })
-  }
+  }*/
 
   const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const { name, number } = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
@@ -114,6 +96,8 @@ const errorHandling = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'There is no object with such id.' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -121,31 +105,6 @@ const errorHandling = (error, req, res, next) => {
 
 app.use(errorHandling)
 
-/*app.post('/api/persons', (req, res) => {
-    const body = req.body
-
-    if (!body.name || !body.number) {
-        return res.status(400).json({ 
-          error: 'Information is missing'
-        })
-    }
-
-    const hasSameName = persons.find(person => body.name === person.name)
-    if (hasSameName) {
-        return res.status(400).json({
-            error: 'Name must be unique'
-        })
-    }
-
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person)
-    res.json(person)
-})*/
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
